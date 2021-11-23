54e4428afc9db59084e282bc57682d0794fded3f
hogg21 <89107241+hogg21@users.noreply.github.com>
2021-11-23 16:27:26 +0200

    Add files via upload

--- task2/calculator.test.js
+++ task2/calculator.test.js
@@ -1,15 +1,21 @@
 import getSum, { getSquaredNumbers,  getOddNumbers } from './calculator';
 
 it('should get squared numbers', () => {
-    const result = getSquaredNumbers([1, 2, 3]);
-    expect(result).toEqual([1, 4, 9]);
+    const result = getSquaredNumbers([1, 2, 3, 4, 5]);
+
+
+    expect(result).toEqual([1, 4, 9, 16, 25]);
 })
 
 it('should keep odd numbers only', () => {
     const result = getOddNumbers([1, 2, 3, 4, 5]);
+
+
     expect(result).toEqual([1, 3, 5]);
 })
 it('should get sum of numbers', () => {
     const result = getSum(8, 4);
+
+    
     expect(result).toEqual(12);
 })
